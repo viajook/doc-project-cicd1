d08102f74cef3272061a51e5b8f39807c20b8e93
viajook <jook@via.dk>
2022-12-15 12:34:26 +0100

    Added Github actions

--- src/main/java/via/doc/devopstoolsmgt/controller/DevOpsPtController.java
+++ src/main/java/via/doc/devopstoolsmgt/controller/DevOpsPtController.java
@@ -22,6 +22,6 @@ public class DevOpsPtController {
 
     @GetMapping("/tools")
     public String getRandomTools() {
-        return "GitLab, GIT";
+        return "GitLab, GitHub Actions";
     }
 }
